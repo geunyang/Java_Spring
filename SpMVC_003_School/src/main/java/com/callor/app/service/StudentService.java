f573391fe4115ddd10557529f909739799e37036
geunyang <gaeballza@gmail.com>
2022-04-22 16:53:28 +0900

    220422오후

---
+++ SpMVC_003_School/src/main/java/com/callor/app/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.callor.app.service;
+
+import java.util.List;
+
+import com.callor.app.domain.StudentVO;
+
+public interface StudentService {
+	// 학생데이터를추가하기
+	public int insert(StudentVO studentVO);
+
+	// 전체 리스트 가져오기
+	public List<StudentVO> selectAll();
+
+	// 특정한 한개의 데이터만 가져오기
+	public StudentVO findById(String stNum);
+
+}
